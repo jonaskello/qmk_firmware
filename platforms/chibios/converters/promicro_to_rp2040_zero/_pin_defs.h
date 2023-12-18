060c084e51ea7b231557a2e14cd21ed43c9af5c8
Jonas Kello <jonas.kello@gmail.com>
2023-12-18 19:18:57 +0100

    Add converter

---
+++ platforms/chibios/converters/promicro_to_rp2040_zero/_pin_defs.h
@@ -0,0 +1,33 @@
+// Copyright 2022 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Right side (front)
+#define D3 0U
+#define D2 1U
+#define D1 2U
+#define D0 3U
+#define D4 4U
+#define C6 5U
+#define D7 6U
+#define E6 7U
+#define B4 8U
+#define B5 9U
+
+// Left side (front)
+//      RAW
+//      GND
+//      VCC
+#define F4 29U
+#define F5 28U
+#define F6 27U
+#define F7 26U
+#define B1 15U
+#define B3 14U
+#define B2 13U
+#define B6 12U
+
+// LEDs
+#define D5 11U
+#define B0 10U
