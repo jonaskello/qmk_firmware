8bceded2f9b1e858ffb81a0cc842ef7b5b1ca4d7
Jonas Kello <jonas.kello@gmail.com>
2024-05-10 18:33:26 +0200

    Fixed info.json

--- keyboards/handwired/dactyl_manuform/4x6_jonaskello/keymaps/jonaskello/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6_jonaskello/keymaps/jonaskello/keymap.c
@@ -40,14 +40,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                           |      |      | |      |      |
      *                                           +-------------+ +-------------+
      */
+    // [_BASE] = LAYOUT(
+    //     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+    //     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    //     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+    //                       KC_LBRC, KC_RBRC,                                                KC_PLUS, KC_EQL,
+    //                                         RAISE,   KC_SPC,             KC_ENT,  LOWER,
+    //                                         KC_TAB,  KC_HOME,            KC_END,  KC_DEL,
+    //                                         KC_BSPC, KC_GRV,             KC_LGUI, KC_LALT
+    // ),
+
     [_BASE] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                          KC_LBRC, KC_RBRC,                                                KC_PLUS, KC_EQL,
-                                            RAISE,   KC_SPC,             KC_ENT,  LOWER,
-                                            KC_TAB,  KC_HOME,            KC_END,  KC_DEL,
-                                            KC_BSPC, KC_GRV,             KC_LGUI, KC_LALT
+                          KC_A, KC_B,                                                KC_PLUS, KC_EQL,
+                                            KC_K,   KC_L,             KC_A,  KC_A,
+                                            KC_M,  KC_N,            KC_A,  KC_A,
+                                            KC_O, KC_P,             KC_A, KC_A
     ),
 
     [_LOWER] = LAYOUT(
@@ -74,4 +84,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void persistent_default_layer_set(uint16_t default_layer) {
     eeconfig_update_default_layer(default_layer);
     default_layer_set(default_layer);
+}
+
+// void keyboard_post_init_user(void) {
+//   // Customise these values to desired behaviour
+//   debug_enable=true;
+//   debug_matrix=true;
+//   //debug_keyboard=true;
+//   //debug_mouse=true;
+// }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif 
+  return true;
 }
